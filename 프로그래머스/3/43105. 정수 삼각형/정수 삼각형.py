9ee5d2c7e609aa9f2dbee53dcc2aab6fff9d0046
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-02-10 10:47:57 +0900

    [level 3] Title: 정수 삼각형, Time: 46.05 ms, Memory: 19.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -4,10 +4,8 @@ def solution(triangle):
     dp = [[-1 for _ in range(len(triangle))] for _ in range(len_tri)]
     
     dp[0][0] = triangle[0][0]
-    dp[1][0] = dp[0][0] + triangle[1][0]
-    dp[1][1] = dp[0][0] + triangle[1][1]
     
-    for i in range(2, len_tri):
+    for i in range(1, len_tri):
         dp[i][0] = dp[i-1][0] + triangle[i][0]
         dp[i][i] = dp[i-1][i] + triangle[i][i]
         
