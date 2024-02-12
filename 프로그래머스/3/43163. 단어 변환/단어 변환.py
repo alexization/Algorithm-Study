07d5ecd42b409d83eff6116319a8eb822adbf4f0
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-02-12 17:29:48 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,35 @@
+def solution(begin, target, words):
+    answer = 0
+    used = []
+    
+    global result_return
+    result_return = []
+    result = 0
+
+    dfs(begin, target, words, used, result)
+    if result_return:
+        answer = min(result_return)
+    else:
+        answer = 0
+    return answer
+
+def dfs(begin, target, words, used, result):
+    if begin == target:
+        result_return.append(result)
+        return
+    
+    for word in words:
+        count = 0
+        for i in range(len(begin)):
+            if begin[i] != word[i]:
+                count += 1
+                
+        if count == 1:
+            if word not in used:
+                used.append(begin)
+                begin = word
+                result += 1
+                dfs(begin, target, words, used, result)
+                
+    return
+                
