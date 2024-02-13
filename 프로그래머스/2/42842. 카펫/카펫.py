5e0a47943207e9670d0adf1392b110ab4edde4bc
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-02-13 11:52:10 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    answer = []
+    y_x = 0
+    y_y = 0
+    
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            y_x = yellow//i
+            y_y = i
+            
+            if y_x * 2 + y_y * 2 + 4 == brown:
+                answer.append(y_x + 2)
+                answer.append(y_y + 2)
+                break
+        
+    return answer
