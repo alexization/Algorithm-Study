209ef3ead4683817d5a50eb1bc49135aae5270f8
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-02-15 23:12:33 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,3 @@
 def solution(n, computers):
     answer = 0
-    for i in range(n):
-        computers[i][i] = 0
-    
-    visited = [False for _ in range(n)]
-    
-    def dfs(x):
-        if visited[x] == True:
-            return
-        visited[x] = True
-        
-        for i in range(n):
-            if computers[x][i] == 1:
-                dfs(i)
-        
-    for i in range(n):
-        if visited[i] == False:
-            dfs(i)
-            answer += 1
-    
     return answer
