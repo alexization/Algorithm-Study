c2e2f9b7c6393e2626e55853ab3e2bfb9a22aea0
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-08-26 18:33:28 +0900

    [Bronze V] Title: WARBOY, Time: 96 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int temp = B / A;
+        temp *= C;
+        System.out.println(temp * 3);
+
+        br.close();
+    }
+}
