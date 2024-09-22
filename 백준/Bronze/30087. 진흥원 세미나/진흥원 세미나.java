9edf091a15052b7d232317445d09cbc87fdedc85
hyoSeok <81959002+alexization@users.noreply.github.com>
2024-09-22 18:52:00 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+        for (int i = 0; i < N; i++) {
+            System.out.println(map.get(br.readLine()));
+        }
+        br.close();
+    }
+}
